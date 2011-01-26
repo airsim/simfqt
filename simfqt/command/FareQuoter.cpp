0cebc245dd1c847c99639acb92029bbc990dd7f9
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-01-26 10:21:17 +0100

    [SimFQT] Integrated latest work done on DSim (on FarePosition, FareQuoter, etc.).

--- simfqt/command/FareQuoter.cpp
+++ simfqt/command/FareQuoter.cpp
@@ -16,7 +16,7 @@
 // SimFQT
 #include <simfqt/SIMFQT_Types.hpp>
 #include <simfqt/bom/AirportPair.hpp>
-#include <simfqt/bom/FarePosition.hpp>
+#include <simfqt/bom/FarePosChannel.hpp>
 #include <simfqt/bom/FareDatePeriod.hpp>
 #include <simfqt/bom/FareRuleFeatures.hpp>
 #include <simfqt/bom/SegmentFeatures.hpp>
@@ -70,7 +70,8 @@ namespace SIMFQT {
 			      lResultParsing);
         
 	if (hasParsingBeenSuccesful) {
-          assert (lResultParsing.size() == 4);
+
+          assert(lResultParsing.size() == 5);
           
           // Create a new travel solution.
           stdair::TravelSolutionStruct lTravelSolution;
@@ -81,8 +82,8 @@ namespace SIMFQT {
 	  lAirportPairStr << lResultParsing.back() << std::endl;
           std::string lAirportPairString = lResultParsing.back();
 
-	  // Search for the fare rules having the same origin and destination 
-	  // airport as the travel solution
+	  // Search for the fare rules having the same origin and
+          // destination airport as the travel solution
 	  const AirportPair* lAirportPair_ptr = stdair::BomManager::
 	    getObjectPtr<AirportPair> (iBomRoot, lAirportPairStr.str());  
 
@@ -96,58 +97,70 @@ namespace SIMFQT {
           }
           
           // Get the position of the booking request.
-          const stdair::CityCode_T& lPosition = iBookingRequest.getPOS();
-          const FarePositionKey lFarePositionKey (lPosition);
+          const stdair::CityCode_T& lPosition =
+            iBookingRequest.getPOS();
+          
+          // Get the booking request channel.
+          const stdair::ChannelLabel_T lChannel =
+            iBookingRequest.getBookingChannel();
+          
+          // Construct the corresponding position-channel primary key.
+          const FarePosChannelKey lFarePosChannelKey (lPosition, lChannel);
           
           // Search for the fare rules having the same position as the travel
           // solution
-          const FarePosition* lFarePosition_ptr = stdair::BomManager::
-            getObjectPtr<FarePosition> (*lAirportPair_ptr, 
-                                        lFarePositionKey.toString());
-
-          if (lFarePosition_ptr == NULL) {
+          const FarePosChannel* lFarePosChannel_ptr =
+            stdair::BomManager::
+            getObjectPtr<FarePosChannel> (*lAirportPair_ptr, 
+                                          lFarePosChannelKey.toString());
+          
+          if (lFarePosChannel_ptr == NULL) {
             STDAIR_LOG_ERROR ("No available fare rule corresponding to the "
                               "position " << lPosition
+                              << ", to the channel " << lChannel
                               << " and to the Origin-Destination pair: "
                               << lAirportPairString);
             throw PositionNotFoundException ("No available fare rule for the "
-                                             "position " + lPosition);
+                                             "position " + lPosition
+                                             + " and the channel " + lChannel);
           }
           
           // Get the date of the segment path.
           const stdair::Date_T lSPDate =
             boost::gregorian::from_simple_string(lResultParsing.at(2));
 
+          // Get the segment boarding time of the segment path.
+          const stdair::Duration_T lSPTime = 
+            boost::posix_time::duration_from_string(lResultParsing.at(3));
+
           // Get the airline code of the segment path.
           const stdair::AirlineCode_T lAirlineCode (lResultParsing.at(0));
 
-          // Get the booking request channel.
-          const stdair::ChannelLabel_T lChannel =
-            iBookingRequest.getBookingChannel();
-
-          // Get the stay duration.
+          // Get the stay duration of the booking request.
           const stdair::DayDuration_T lStayDuration =
             iBookingRequest.getStayDuration();
 
-          // Get the request date-time.
+          // Get the booking request date time.
           const stdair::DateTime_T lRequestDateTime =
             iBookingRequest.getRequestDateTime();
 
-          // Set a random (for now) price.
-          stdair::PriceValue_T lPrice  = 600.0;
-          stdair::DayDuration_T lAdvancePurchase;
+          // Declare fare parameters to determine.
+          stdair::PriceValue_T lPrice;
           stdair::SaturdayStay_T lSaturdayStay;
           stdair::ChangeFees_T lChangeFees;
           stdair::NonRefundable_T lNonRefundable;
       
           // Search for the fare rules having a corresponding date range.
-          const FareDatePeriodList_T& lFareDatePeriodList = stdair::BomManager::
-            getList<FareDatePeriod> (*lFarePosition_ptr);
+          const FareDatePeriodList_T& lFareDatePeriodList =
+            stdair::BomManager::
+            getList<FareDatePeriod>(*lFarePosChannel_ptr);
           
           for (FareDatePeriodList_T::const_iterator itDateRange =
                  lFareDatePeriodList.begin();
-               itDateRange != lFareDatePeriodList.end(); ++itDateRange ) {
-            const FareDatePeriod* lcurrentFareDatePeriod_ptr = *itDateRange ;
+               itDateRange != lFareDatePeriodList.end();
+               ++itDateRange) {
+            const FareDatePeriod* lcurrentFareDatePeriod_ptr =
+              *itDateRange ;
             assert (lcurrentFareDatePeriod_ptr != NULL);
             
             if (lcurrentFareDatePeriod_ptr->isDepartureDateValid(lSPDate)
@@ -164,12 +177,17 @@ namespace SIMFQT {
                 const FareRuleFeatures* lcurrentFareRuleFeatures_ptr =
                   *itFareFeatures;
                 assert (lcurrentFareRuleFeatures_ptr != NULL);
-    
+
+                lPrice = lcurrentFareRuleFeatures_ptr->getFare();
+                lSaturdayStay = lcurrentFareRuleFeatures_ptr->getSaturdayStay();
+                lChangeFees = lcurrentFareRuleFeatures_ptr->getChangeFees();
+                lNonRefundable = lcurrentFareRuleFeatures_ptr->getRefundableOption();
+                 
                 // Search for the fare rules having a corresponding airline.
                 const SegmentFeaturesList_T& lSegmentFeaturesList =
                   stdair::BomManager::
-                  getList<SegmentFeatures> (*lcurrentFareRuleFeatures_ptr);
-
+                  getList<SegmentFeatures>(*lcurrentFareRuleFeatures_ptr);  
+                
                 for (SegmentFeaturesList_T::const_iterator itSegmentFeatures =
                        lSegmentFeaturesList.begin();
                      itSegmentFeatures != lSegmentFeaturesList.end();
@@ -180,11 +198,9 @@ namespace SIMFQT {
                   
                   if (lcurrentSegmentFeatures_ptr->getAirlineCode()
                       == lAirlineCode) {
-                    // TODO: do not hardcode the price
-                    lPrice  = 1200.0;
                     
                     // Set the travel solution price.
-                    lTravelSolution.setFare (lPrice);
+                    lTravelSolution.setFare(lPrice);
 
                     // DEBUG
                     STDAIR_LOG_DEBUG ("The price is "
